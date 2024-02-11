2ee7f46c7ad2bba7a913bf23e91d6cc796a3314c
Andrew O'Hara <ohara.andrew314@gmail.com>
2024-02-10 19:28:55 -0500

    Step 6: Replace Logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,7 @@ repositories {
 dependencies {
     implementation("org.http4k:http4k-connect-amazon-dynamodb:5.6.11.0")
     implementation("org.http4k:http4k-serverless-lambda:5.13.6.0")
-    implementation("ch.qos.logback:logback-core:1.4.14")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("org.slf4j:slf4j-simple:2.0.11")
     implementation("se.ansman.kotshi:api:2.15.0")
     ksp("se.ansman.kotshi:compiler:2.15.0")
 }
