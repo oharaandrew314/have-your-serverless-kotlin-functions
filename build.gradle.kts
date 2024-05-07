4b6e8c628bffc99811e955433ac7631bd3c368a0
Andrew O'Hara <ohara.andrew314@gmail.com>
2024-05-07 11:04:53 -0400

    minimize jar with shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     id("com.google.devtools.ksp") version "1.9.22-1.0.17"
+    id("io.github.goooler.shadow") version "8.1.7"
 }
 
 repositories {
@@ -10,9 +11,9 @@ repositories {
 dependencies {
     implementation("org.http4k:http4k-connect-amazon-dynamodb:5.6.11.0")
     implementation("org.http4k:http4k-serverless-lambda:5.13.6.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.23.1")
     implementation("se.ansman.kotshi:api:2.15.0")
     ksp("se.ansman.kotshi:compiler:2.15.0")
+    implementation("org.slf4j:slf4j-simple:2.0.11")
 }
 
 tasks.test {
@@ -20,4 +21,9 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+tasks.shadowJar {
+    minimize {
+        exclude(dependency("org.slf4j:.*:.*"))
+    }
 }
